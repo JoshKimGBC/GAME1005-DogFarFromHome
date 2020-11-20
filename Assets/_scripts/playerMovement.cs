45c14226dd104eee3f92f927a0bde314abd55ca3
Nick Oostdyk <NicholasOostdyk@gmail.com>
2020-11-19 22:27:33 -0500

    added the most basics of movement

---
+++ Assets/_scripts/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    private float h, v;
+
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        h = Input.GetAxisRaw("Horizontal");
+        Debug.Log(h);
+    }
+
+    private void FixedUpdate() {
+
+        transform.position += new Vector3(h * speed * Time.deltaTime, 0, 0);
+    }
+}
